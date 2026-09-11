b7d837f8b1757fe8ce230df41c308338b6d54e70
Nitish Maity <maitynitish1@gmail.com>
2026-09-11 19:32:09 +0530

    LeetCode: update java/best_time_to_buy_and_sell_stock.java

--- java/best_time_to_buy_and_sell_stock.java
+++ java/best_time_to_buy_and_sell_stock.java
@@ -3,7 +3,7 @@
 // Language: java
 // Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 // Synced by: LinkCode
-// Date: 9/11/2026, 7:26:26 PM
+// Date: 9/11/2026, 7:32:05 PM
 // ======================================
 
 
@@ -12,20 +12,18 @@ class Solution {
         if(prices.length==0){
             return 0;
         }
-        int min=prices[0];
-        int[] profit=new int[prices.length];
-        for(int i=1;i<prices.length;i++){
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<prices.length;i++){
             if(prices[i]<min){
                 min=prices[i];
             }
-            profit[i]=prices[i]-min;
-        }
-        int max=0;
-        for(int i=0;i<profit.length;i++){
-            if(max<profit[i]){
-                max=profit[i];
+            if(prices[i]-min>max){
+                max=prices[i]-min;
             }
+            
         }
+      
         return max;
     }
 }
