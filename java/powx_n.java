145c246924d3273d45517ece1047278efb00970d
Nitish Maity <maitynitish1@gmail.com>
2026-09-14 19:26:14 +0530

    LeetCode: update java/powx_n.java

--- java/powx_n.java
+++ java/powx_n.java
@@ -3,21 +3,12 @@
 // Language: java
 // Link: https://leetcode.com/problems/powx-n/
 // Synced by: LinkCode
-// Date: 9/14/2026, 7:25:29 PM
+// Date: 9/14/2026, 7:26:13 PM
 // ======================================
 
 
 class Solution {
     public double myPow(double x, long n) {
-        // if(n==0){
-        //     return 1;
-        // }
-        // if(n<0){
-        //     x=1/x;
-        //     n=Math.abs(n);
-        // }
-        // return x*myPow(x,n-1);
-
         if(n<0){
             x=1/x;
             n=-n;
