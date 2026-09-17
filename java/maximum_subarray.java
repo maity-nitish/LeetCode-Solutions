c8dc2017e18ffeb4d14fe269eca1efa79d3e511d
Nitish Maity <maitynitish1@gmail.com>
2026-09-17 19:34:55 +0530

    LeetCode: update java/maximum_subarray.java

---
+++ java/maximum_subarray.java
@@ -0,0 +1,23 @@
+// ======================================
+// LeetCode Problem: maximum subarray
+// Language: java
+// Link: https://leetcode.com/problems/maximum-subarray/
+// Synced by: LinkCode
+// Date: 9/17/2026, 7:34:54 PM
+// ======================================
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int cs=nums[0];
+        int ms=nums[0];
+        for(int i=1;i<nums.length;i++){
+            cs=Math.max(nums[i],cs+nums[i]);
+            ms=Math.max(cs,ms);
+        }
+        return ms;
+    }
+}
